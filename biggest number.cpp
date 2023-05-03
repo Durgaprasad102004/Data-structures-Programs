b4fd2b0c044a1a974cb3ac397deea47701d4a064
Durgaprasad102004 <118253294+Durgaprasad102004@users.noreply.github.com>
2023-05-03 15:56:06 +0530

    Add files via upload

---
+++ biggest number.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the values:");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if(a>b)
+	{
+		printf("a is the biggest number");
+	}
+	else
+	{
+		printf("b is the biggest number");
+	}
+}
